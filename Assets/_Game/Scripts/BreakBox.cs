66257b6ad45fdd44cdd1d0977b57b80cb209c3c1
Daniel Manganaro <manganaro.daniel@gmail.com>
2019-09-27 14:34:14 +0800

    Project Upload

---
+++ Assets/_Game/Scripts/BreakBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBox : MonoBehaviour
+{
+    public GameObject brokenBox;
+
+    public void Break()
+    {
+        GameObject broken = Instantiate(brokenBox, transform.position, transform.rotation);
+        Rigidbody[] rbs = broken.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rb in rbs) {
+            rb.AddExplosionForce(150, transform.position, 30);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Game/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public bool activated;
+    public float rotationSpeed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated) {
+            transform.localEulerAngles += Vector3.forward * rotationSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+
+        activated = false;
+        GetComponent<Rigidbody>().Sleep();
+        GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Breakable")) {
+            if (other.GetComponent<BreakBox>() != null) {
+                other.GetComponent<BreakBox>().Break();
+            }
+        }
+    }
+}
